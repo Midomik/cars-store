347baa9cb3a751713c180602da2aed688907bddd
Midomik <80357093+Midomik@users.noreply.github.com>
2024-03-08 12:13:53 +0000

    Deploying to gh-pages from @ Midomik/cars-store@f2b697251368208f5606d4b21453a30ec7cbf5b6 🚀

---
+++ static/js/212.dcf496bb.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkcars_store=self.webpackChunkcars_store||[]).push([[212],{212:function(e,r,s){s.r(r);s(791);var t=s(184);r.default=function(){return(0,t.jsx)("div",{children:"FavoritePage"})}}}]);
+//# sourceMappingURL=212.dcf496bb.chunk.js.map
